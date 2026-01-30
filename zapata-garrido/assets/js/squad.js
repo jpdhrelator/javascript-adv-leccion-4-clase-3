0508aac60fcecc8af499878aa3a7e5adb8bd5732
AmasandoPan <jeremy.garrido.r@gmail.com>
2026-01-29 22:12:54 -0300

    se carga squad js

--- zapata-garrido/assets/js/squad.js
+++ zapata-garrido/assets/js/squad.js
@@ -4,12 +4,16 @@ async function deployWeapon(weaponType){
     try {
         await esperar(2000);
         const fallar = Math.random() < 0.3; // 30%
-        if (fallo){
-            
+        if (fallar){
+            console.log(`Fallo al cargar ${weaponType}`);
         }
-
         return `Arma ${weaponType} cargada y lista`
     } catch (error) {
-        
+        throw error.msg;
     }
+}
+
+async function moveSquad(sector) {
+    await esperar(3000);
+    return `Escuadrón arribado al ${sector}. Perimetro seguro`;
 }
